d8aed874f6a43baa8fb516baf620ac957e21c94c
umbertonasta <115807344+umbertonasta@users.noreply.github.com>
2024-05-21 02:04:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Aggiungi event listener per scroll smooth
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
